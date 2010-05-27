9e9f021ba78ffdcb5a2b6a120400a9c2166eef03
Joshua Oreman <oremanj@rwcr.net>
2010-05-26 17:52:44 -0700

    [linker] Use weak definitions instead of weak declarations
    This removes the need for inline safety wrappers, marginally
    reducing the size penalty of weak functions, and works around an
    apparent binutils bug that causes undefined weak symbols to not
    actually be NULL when compiling with -fPIE (as EFI builds do).
    
    A bug in versions of binutils prior to 2.16 (released in 2005)
    will cause same-file weak definitions to not work with those
    toolchains. Update the README to reflect our new dependency on
    binutils >= 2.16.
    
    Signed-off-by: Joshua Oreman <oremanj@rwcr.net>

--- src/net/udp/dhcp.c
+++ src/net/udp/dhcp.c
@@ -1410,6 +1410,11 @@ static struct sockaddr dhcp_peer = {
 };
 
 /**
+ * Get cached DHCPACK where none exists
+ */
+__weak void get_cached_dhcpack ( void ) {}
+
+/**
  * Start DHCP state machine on a network device
  *
  * @v job		Job control interface
