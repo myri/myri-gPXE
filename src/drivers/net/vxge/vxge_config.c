ipxe/ipxe

[ethernet] Move Ethernet MAC address checking routines to ethernet.h
Originally-fixed-by: Faur Andrei <da3drus@gmail.com>
Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/drivers/net/vxge/vxge_config.c
@@ -18,6 +18,7 @@ FILE_LICENCE(GPL2_ONLY);
 #include <stdio.h>
 #include <ipxe/malloc.h>
 #include <ipxe/iobuf.h>
+#include <ipxe/ethernet.h>
 #include <byteswap.h>
 
 #include "vxge_traffic.h"

myri/myri-gPXE

Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

src/drivers/net/vxge/vxge_config.c
@@ -18,6 +18,7 @@ FILE_LICENCE(GPL2_ONLY);
 #include <stdio.h>
 #include <gpxe/malloc.h>
 #include <gpxe/iobuf.h>
+#include <gpxe/ethernet.h>
 #include <byteswap.h>
 
 #include "vxge_traffic.h"